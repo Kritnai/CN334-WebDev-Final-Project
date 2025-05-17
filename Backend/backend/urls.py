d8aee4a4b5f1b46378fed15e87d49f34b26d970b
MultiThreadJava <KritsanaiHosang@gmail.com>
2025-05-17 14:22:10 +0700

    fix back system

--- Backend/backend/urls.py
+++ Backend/backend/urls.py
@@ -17,7 +17,12 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+from django.conf import settings
+from django.conf.urls.static import static
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('sheets_service.urls')), # รวม URLs ของแอปของคุณไว้ใต้ /api/
 ]
+# ✅ เสริมนี้เข้าไป (แม้จะ DEBUG=False)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
